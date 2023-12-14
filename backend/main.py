27d2820471ac577db718450afe5809b56df5e263
tame0001 <thirawat.tam@gmail.com>
2023-12-14 22:44:18 +0700

    restructure the folder

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,12 @@
-import fastapi
+from fastapi import FastAPI
+
+description = """
+Goose project API.
+"""
+
+app = FastAPI(
+    title="Goose API",
+    description=description,
+    version="0.0.1",
+    contact={"name": "Tam Bureetes", "email": "thirawat.tam@gmail.com"},
+)
