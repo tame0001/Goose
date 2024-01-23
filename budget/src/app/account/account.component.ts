e08aec02c574e4b2c06c0635ef910eac2ab03f97
tame0001 <thirawat.tam@gmail.com>
2024-01-23 15:03:58 -0500

    enable animation

--- budget/src/app/account/account.component.ts
+++ budget/src/app/account/account.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AccountRead } from './interfaces';
 import { AccountService } from './account.service';
@@ -9,19 +10,26 @@ import { AccountService } from './account.service';
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [MatTableModule, CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css',
 })
 export class AccountComponent {
   public allAccounts = new MatTableDataSource<AccountRead>();
   public columnsToDisplay: string[] = ['name', 'balance'];
+  public isLoading = true;
 
   constructor(private _accountService: AccountService) {}
 
   ngOnInit() {
     this._accountService.getAllAccount().subscribe((data) => {
       this.allAccounts = new MatTableDataSource<AccountRead>(data);
+      this.isLoading = false;
     });
   }
 }
--- budget/src/app/app.config.ts
+++ budget/src/app/app.config.ts
@@ -3,12 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideNoopAnimations(),
+    provideAnimations(),
   ],
 };
